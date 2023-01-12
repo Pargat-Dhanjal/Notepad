f8d60e9c28e2821e1d03efd9fc5f014fab9d35c9
Pargat-Dhanjal <pargat14@hotmail.com>
2023-01-12 23:49:01 +0530

    fix: formating and changed title

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function Note(props) {
   return (
     <div className="note">
       <h1>{props.heading}</h1>
       <p>{props.content}</p>
-      <button onClick={()=> {
-        props.delete(props.id);
-      }}><DeleteIcon className="icons" /></button>
+      <button
+        onClick={() => {
+          props.delete(props.id);
+        }}
+      >
+        <DeleteIcon className="icons" />
+      </button>
     </div>
   );
 }
-
 
 export default Note;
